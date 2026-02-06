9be7e9f8658980668973b398f4761a8fafe150e9
ISRAEL FUJIMOTO <25203688@CFP127ALUNO.LOCAL>
2026-02-06 15:29:48 -0300

    Termino da atividade

--- app.js
+++ app.js
@@ -23,16 +23,60 @@ entradaDeDados.question("Digite o seu nome: ", function(nome){
                 
                 let taxaDeJuros = taxa
 
-                entradaDeDados.question("insira o tempo de pagamento: ", function(tempo){
+                entradaDeDados.question("O senhor(a) deseja pagar em anos ou meses? \nCaso seja em meses digite [1] se for em anos digite [2]: ", function(opcao){
 
-                    let tempoDePagamento = tempo
+                    let opcaoDeCompra = opcao
+                
 
-                    let montante = Number(valor) * (1 + Number(taxa/100)) ** Number(tempo)
-                    console.log(montante)
+                    entradaDeDados.question("insira em quanto tempo o(a) senhor(a) deseja pagar: ", function(tempo){
+
+                        let tempoDePagamento = tempo
+                    
+
+                        if(nome == "" || produto == "" || valor == "" || taxa == "" || tempo == ""){
+                        console.log("ERRO: O(S) CAMPO(s) DEVE(M) SER PREENCHIDO(S)!!!")
+
+                        }else if(isNaN(valor) + isNaN(taxa) + isNaN(tempo)){
+                        console.log("ERRO: SOMENTE NUMEROS SÃO PERMITIDOS NOS CAMPOS!!! ")
+
+                        }else if(opcao == 1){
+
+                            let montante = Number(valor) * (1 + Number(taxa/100)) ** Number(tempo)
+
+                                let valorDoAcrecimo = Number(montante) - Number(valor)
+
+                                console.log(`******************* [VIVA MODA] *******************
+
+Muito obrigado por realizar a sua compra conosco Sr(a) ${nome}
+A compra do produto ${produto}, tem um valor de: ${valor}
+A sua compra será parcelada em ${taxa} vezes e o Sr(a) pagará: ${montante.toFixed(2)}
+O acréscimo realizado ao valor de: ${valor} será de ${valorDoAcrecimo.toFixed(2)}
+                                                                                            
+Muito obrigado por escolher a VIVA MODA!!!
+                                                                    
+***************************************************`)
+       
+                        }else if(opcao == 2){
+                            
+                            let montante = Number(valor) * (1 + Number(taxa/100)) ** Number(tempo*12)
+
+                                let valorDoAcrecimo = Number(montante) - Number(valor)
+
+                                console.log(`******************* [VIVA MODA] *******************
+
+Muito obrigado por realizar a sua compra conosco Sr(a) ${nome}
+A compra do produto ${produto}, tem um valor de: ${valor}
+A sua compra será parcelada em ${taxa} vezes e o Sr(a) pagará: ${montante.toFixed(2)}
+O acréscimo realizado ao valor de: ${valor} será de ${valorDoAcrecimo.toFixed(2)}
+                                                        
+Muito obrigado por escolher a VIVA MODA!!!
+                                
+***************************************************`)
+                        }
+
+                    })
                 })
             })
         })    
     })
 })
-
-
